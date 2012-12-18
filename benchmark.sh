03c52da946b934072474c55529bfcda4288ab18a
Jan van der Lugt <janlugt@gmail.com>
2012-12-18 18:07:34 +0100

    -

--- benchmark.sh
+++ benchmark.sh
@@ -9,24 +9,24 @@ do
 	slots=1
 	for n in 1 2 4 8 16 24 32      	   
 	do
-		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii ~/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
+		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii /home/janlugt/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
 	done
 	
 	slots=2
 	for n in 20 24 28 32	   
 	do
-		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii ~/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
+		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii /home/janlugt/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
 	done
 	
 	slots=3
 	for n in 32
 	do
-		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii ~/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
+		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii /home/janlugt/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
 	done
 	
 	slots=4
 	for n in 32
 	do
-		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii ~/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
+		bin/run-bunch.sh -slots ${slots} ${n} ${algorithm} svcii /home/janlugt/graphs/${graph}_svcii_$(($n * $slots)) &> results/${graph}_${algorithm}_${slots}_${n}
 	done
 done
